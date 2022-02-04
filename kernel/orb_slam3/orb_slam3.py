d45b925c01808302a75586e5cf0e8e3c5d47c0fa
370620267@qq.com <huijiezh@umich.edu>
2022-02-03 20:40:14 -0500

    update orb_slam3

--- kernel/orb_slam3/orb_slam3.py
+++ kernel/orb_slam3/orb_slam3.py
@@ -3,5 +3,4 @@ if __name__ == "__main__":
     import os
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
     from build.orb3_extension import orb3_slam_recon
-    # orb3_slam_recon(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], float(sys.argv[6]), float(sys.argv[7]))
-    orb3_slam_recon(sys.argv[1], "/home/huijie/research/progresslabeller/ORB_SLAM3/Examples/RGB-D/test.yaml", sys.argv[3], sys.argv[4], sys.argv[5], float(sys.argv[6]), float(sys.argv[7]))
+    orb3_slam_recon(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], float(sys.argv[6]), float(sys.argv[7]))
